840b2ae931d37d243ac4d16f4fedd11f4517ef81
bohan24 <55724991+bohan24@users.noreply.github.com>
2022-10-03 10:22:35 +0800

    Update CustomerParameter.cs

--- CRUDModel/Parameters/CustomerParameter.cs
+++ CRUDModel/Parameters/CustomerParameter.cs
@@ -4,43 +4,7 @@ namespace CRUDModel.Parameters
 {
     public class CustomerParameter
     {
-        public class getCustomerPara
-        {
-            /// <summary>
-            /// Customer Name
-            /// </summary>
-            public string? CustName { get; set; }
-
-            /// <summary>
-            /// Customer Birthday
-            /// </summary>
-            public string? CustBirthday { get; set; }
-
-            /// <summary>
-            /// Customer Phone
-            /// </summary>
-            public string? CustPhone { get; set; }
-
-            /// <summary>
-            /// Current Page Index
-            /// </summary>
-            [Range(1, short.MaxValue)]
-            public short Page { get; set; } = 1;
-
-            /// <summary>
-            /// Pens per page
-            /// </summary>
-            [Range(1, short.MaxValue)]
-            public short PageSize { get; set; } = 20;
-        }
-
-        public class returnToAiCustPara
-        {
-            /// <summary>
-            /// Customer ID
-            /// </summary>
-            public string CustId { get; set; }
-        }
+       
 
     }
 }
