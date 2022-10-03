9a3e888277bd0b651838fb4a820064679c16e484
bohan24 <55724991+bohan24@users.noreply.github.com>
2022-10-03 10:21:19 +0800

    Update CustomerDto.cs

--- CRUDModel/Dtos/CustomerDto.cs
+++ CRUDModel/Dtos/CustomerDto.cs
@@ -1,20 +1,4 @@
 ﻿namespace CRUDModel.Dtos
 {
-    public class CustomerListDto { 
-        public List<CustomerDto> customer { get; set; }
-    }
 
-    public class CustomerDto
-    {
-        public string CustId { get; set; }
-
-        public string CustName { get; set; }
-
-        public string CustSex { get; set; }
-
-        public string CustBirthday { get; set; }
-
-        public string CustPhone { get; set; }
-
-    }
 }
