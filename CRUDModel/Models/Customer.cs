9fe1d40ad76d1d1b2e685751991ee451542ac2e2
bohan24 <55724991+bohan24@users.noreply.github.com>
2022-10-03 10:21:43 +0800

    Update Customer.cs

--- CRUDModel/Models/Customer.cs
+++ CRUDModel/Models/Customer.cs
@@ -1,23 +1,4 @@
 ﻿namespace CRUDModel.Models
 {
-    public class Customer
-    {
-        public string no { get; set; }
-        public string name { get; set; }
-        public string phoneno { get; set; }
-        public string birthday { get; set; }
-        public decimal beautician { get; set; }
-        public string sex { get; set; }
-        public string pid { get; set; }
-        public string address { get; set; }
-        public string consultation_no { get; set; }
-        public string attention_no { get; set; }
-        public int AIQ { get; set; }
-        public string AIQ_Med_no { get; set; }
-        public string Leisure { get; set; }
-        public string Trait_color { get; set; }
-        public string DrLee { get; set; }
-        public string photo { get; set; }
-        public string VIP { get; set; }
-    }
+
 }
