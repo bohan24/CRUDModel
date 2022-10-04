e5e4843ec9cb8f5a9e29fb728dc02d64107de849
bohan24 <55724991+bohan24@users.noreply.github.com>
2022-10-04 08:54:06 +0800

    Add files via upload

---
+++ CRUDModel/Services/HttpRequestService.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace CRUDModel.Services
+{
+    public class HttpRequestService
+    {
+        public async Task<dynamic> post(string url, object param,dynamic header=null)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(url));
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                if(header!=null)
+                {
+                    foreach(var row in header)
+                    {
+                        request.Headers.Add(row.Key, row.Value);
+                    }
+                }
+                byte[] bs = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(param));
+                request.ContentLength = bs.Length;
+                request.GetRequestStream().Write(bs, 0, bs.Length);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream());
+                string result = sr.ReadToEnd();
+                Console.WriteLine("debug:" + result);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+
+        }
+
+        public async Task<dynamic> get(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            //request.ContentType = "application/json";
+            //byte[] bs = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(param));
+            //request.ContentLength = bs.Length;
+            //request.GetRequestStream().Write(bs, 0, bs.Length);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            string result = sr.ReadToEnd();
+            Console.WriteLine("debug:" + result);
+            return result;
+
+        }
+
+    }
+}
